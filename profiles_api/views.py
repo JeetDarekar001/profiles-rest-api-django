2041d5c037bd5d2e34f30b50a2e6c9d510818405
Abhijit <darekarabhijit63@gmail.com>
2022-01-19 20:27:36 +0530

    Created adn addedd hello-api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
+from operator import imod
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class helloApiView(APIView):
+    """Test APIView"""
+
+    def get(self,request,format=None):
+        """Returns a list of APIView Features"""
+
+        an_apiview=[
+        'Uses HTTP Menthofs as functiosn (get, post, patch)',
+        'Is similar to a tradational Django view'
+        'Gives you the most control over application logic',
+        'Is mapped manually to URLs'
+        ]
+
+        return Response({'message':'Hello!','an_apiview':an_apiview})

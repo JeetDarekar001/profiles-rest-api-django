841b755f9f922ee7322ddf536c1cc3aca51f876b
Abhijit <darekarabhijit63@gmail.com>
2022-01-20 11:56:45 +0530

    Added Filter to the Code

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from multiprocessing import AuthenticationError
 from operator import imod
 from os import stat
 from urllib import response
+from rest_framework import filters
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -120,4 +121,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email')
+    
